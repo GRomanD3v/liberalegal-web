99373c9030b0a0b763a1f4e10db5ce7bbf4e9cbe
Gonzalo Roman <gonzalo.romanreyes@gmail.com>
2026-04-22 13:46:59 -0400

    Styles: corrección en padding top

--- app/pages/contacto.vue
+++ app/pages/contacto.vue
@@ -19,7 +19,7 @@
     </div>
 
     <!-- Contenido -->
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-6xl mx-auto px-4 pt-16 sm:px-6 lg:px-8">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
 
         <!-- Info Column -->
--- app/pages/servicios.vue
+++ app/pages/servicios.vue
@@ -19,7 +19,7 @@
     </div>
 
     <!-- Contenido -->
-    <div class="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-7xl mx-auto px-4 pt-16 sm:px-6 lg:px-8">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
         
         <!-- Servicio 1: Defensa Integral de deudores -->
